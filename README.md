0b5784834c24a8df336db386253d5c8befbc1baf
TylerBloom <tylerbloom2222@gmail.com>
2021-12-26 13:02:23 -0600

    Working on port from python API, old code for temp reference. WIP

---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+This is an SDK for [TriceBot]("https://github.com/djpiper28/CockatriceTournamentBot") in Rust.
--- src/game_settings.rs
+++ src/game_settings.rs
@@ -1,3 +1,25 @@
 
+use serde::{Serialize,Deserialize};
+
+#[derive(Debug,Copy,Clone,Serialize,Deserialize)]
 pub struct GameSettings {
+    spectators_allowed: bool,
+    spectators_need_password: bool,
+    spectators_can_chat: bool,
+    spectators_can_see_hands: bool,
+    only_registered: bool,
+    player_deck_verification: bool,
+}
+
+impl GameSettings {
+    pub fn new() -> Self {
+        GameSettings {
+            spectators_allowed: false,
+            spectators_need_password: false,
+            spectators_can_chat: false,
+            spectators_can_see_hands: false,
+            only_registered: false,
+            player_deck_verification: false,
+        }
+    }
 }
